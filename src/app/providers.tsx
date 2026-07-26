cff2fc3801dab14f86315bdb1d5b895cd312f61b
Mariam Zubair Malik <mariamzubairmalik016@gmail.com>
2026-07-26 17:13:15 +0500

    Fix orphaned Service Worker for Next.js App Router

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,14 @@
 
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+import { registerOfflineWorker } from "../services/registerOfflineWorker";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    registerOfflineWorker();
+  }, []);
+
   return (
     <SessionProvider>
       {children}
